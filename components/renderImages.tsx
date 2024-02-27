19ab3c4a929dce7388c5b6815f3c5772c5c39174
Robert Talamantez <robert.m.talamantez@gmail.com>
2024-02-26 16:34:22 -0800

    WIP - integrate GCP

--- components/renderImages.tsx
+++ components/renderImages.tsx
@@ -1,3 +1,8 @@
+import { UploadImage } from "../UploadImage.ts"
+
+const token =
+  "ya29.a0AfB_byDPGnh-Qr04DRE0lH4VDvkQpXMj0wOvMw0vjtBsWCcxhRDJLUiqwibAxIIK7KNggKwLaNnTU2E0lLV4vbCiqs-te1OwjeMB69Uo1NCFKO8eDFu7OQ71Ny0u5fJ1ttK9cS9QS7sHklY_Foz-lOImaniivX3klBx6aCgYKAQwSARISFQHGX2MifwvXp0o278A1YyPazNY6FQ0171";
+const bucket = "nami-resource-roadmap";
 
 export const renderImages = (fileUrls) => {
   console.log(fileUrls)
@@ -6,8 +11,10 @@ export const renderImages = (fileUrls) => {
       {fileUrls && fileUrls.map((fileUrl) => (
         <div key={fileUrl.fileUrl}>
           <img src={fileUrl.fileUrl} />
+          <button onClick={UploadImage(bucket,token,fileUrl.fileUrl)}>Save</button>
         </div>
       ))}
     </div>
   )
 }
+
