3a0537e30d76d132b0e67353684f8c25d1639bfe
Robert Talamantez <robert.m.talamantez@gmail.com>
2024-03-18 14:42:39 -0700

    WIP - add default image

--- islands/TodoListView.tsx
+++ islands/TodoListView.tsx
@@ -11,12 +11,13 @@ interface LocalMutation {
 type Image = {
   fileUrl: string;
 };
-const ImageContext = createContext(null);
+
+const ImageContext = createContext("https://consciousrobot-956159009.imgix.net/logo.png");
+
 export default function TodoListView(
   props: { initialData: TodoList; latency: number },
 ) {
   const [data, setData] = useState(props.initialData);
-  const [myImgUrl, setMyImgUrl] = useState("https://consciousrobot-956159009.imgix.net/logo.png");
 
   const [dirty, setDirty] = useState(false);
   const localMutations = useRef(new Map<string, LocalMutation>());
@@ -24,9 +25,7 @@ export default function TodoListView(
   const busy = hasLocalMutations || dirty;
   const [adding, setAdding] = useState(false);
 
-  useEffect(() => {
-    console.log(`myImgUrl Changed: ${myImgUrl}`)
-  }, [myImgUrl]);
+  const [myImgUrl, setMyImgUrl] = useState("https://consciousrobot-956159009.imgix.net/logo.png");
 
   useEffect(() => {
     let es = new EventSource(window.location.href);
@@ -112,51 +111,53 @@ export default function TodoListView(
   );
 
   return (
-    <div class="flex gap-2 w-full items-center justify-center py-4 xl:py-16 px-2">
-      <div class="rounded w-full xl:max-w-xl">
-        <div class="flex flex-col gap-4 pb-4">
-          <div class="flex flex-row gap-2 items-center">
-            <h1 class="font-bold text-xl">Local Resource List</h1>
-            <div
-              class={`inline-block h-2 w-2 ${
-                busy ? "bg-yellow-600" : "bg-green-600"
-              }`}
-              style={{ borderRadius: "50%" }}
-            >
+    <ImageContext.Provider value={myImgUrl}>
+      <div class="flex gap-2 w-full items-center justify-center py-4 xl:py-16 px-2">
+        <div class="rounded w-full xl:max-w-xl">
+          <div class="flex flex-col gap-4 pb-4">
+            <div class="flex flex-row gap-2 items-center">
+              <h1 class="font-bold text-xl">Local Resource List</h1>
+              <div
+                class={`inline-block h-2 w-2 ${
+                  busy ? "bg-yellow-600" : "bg-green-600"
+                }`}
+                style={{ borderRadius: "50%" }}
+              >
+              </div>
+            </div>
+            <div class="flex">
+              <p class="opacity-50 text-sm">
+                Local Resources
+              </p>
+            </div>
+            <div class="flex">
+              <input
+                class="border rounded w-full py-2 px-3 mr-4"
+                placeholder="Name your resource here"
+                ref={addTodoInput}
+              />
+              <button
+                class="p-2 bg-blue-600 text-white rounded disabled:opacity-50"
+                onClick={addTodo}
+                disabled={adding}
+              >
+                Add
+              </button>
             </div>
           </div>
-          <div class="flex">
-            <p class="opacity-50 text-sm">
-              Local Resources
-            </p>
+          <div>
+            {data.items.map((item) => (
+              <TodoItem
+                key={item.id! + ":" + item.versionstamp!}
+                item={item}
+                save={saveTodo}
+                imgUrl={myImgUrl}
+              />
+            ))}
           </div>
-          <div class="flex">
-            <input
-              class="border rounded w-full py-2 px-3 mr-4"
-              placeholder="Name your resource here"
-              ref={addTodoInput}
-            />
-            <button
-              class="p-2 bg-blue-600 text-white rounded disabled:opacity-50"
-              onClick={addTodo}
-              disabled={adding}
-            >
-              Add
-            </button>
-          </div>
-        </div>
-        <div>
-          {data.items.map((item) => (
-            <TodoItem
-              key={item.id! + ":" + item.versionstamp!}
-              item={item}
-              save={saveTodo}
-              imgUrl={myImgUrl}
-            />
-          ))}
         </div>
       </div>
-    </div>
+    </ImageContext.Provider>
   );
 }
 
@@ -167,30 +168,37 @@ function TodoItem(
     save: (item: TodoListItem, text: string | null, imgUrl: string) => void;
   },
 ) {
-  const [files, setFiles] = useState("");
-  const onFileSelect: Event = (event) => {
-    setFiles(event.target.files);
-  };
 
   const input = useRef<HTMLInputElement>(null);
   const imageLayout = useRef<HTMLInputElement>(null);
+  const fileInput = useRef<HTMLInputElement>(null);
 
   const [editing, setEditing] = useState(false);
   const [busy, setBusy] = useState(false);
 
-  const doSave = useCallback(() => {
-    if (!input.current) return;
-    if (!imageLayout.current) return;
-    if (!imageLayout.current.files) return;
-    setBusy(true);
-    const newImgUrl = URL.createObjectURL(imageLayout.current.files[0]);
+  const [files, setFiles] = useState("");
 
-    console.log("\n\n*******  SAVING  *******\n\n")
+  const onFileSelect: Event = (event) => {
+    console.log(event.target.files)
+    console.log("files", files)
+    setFiles(event.target.files);
+  };
+
+  const doSave = useCallback(() => {
+    console.log("\n\n*******  saving  *******\n\n");
+    if (!input.current || !fileInput.current || !fileInput.current.files ){
+      console.log('missing data')
+      return;
+    }
+    setBusy(true);
+
+    const newImgUrl = URL.createObjectURL(fileInput.current.files[0]);
+    console.log("newImgUrl", newImgUrl)
+    console.log("\n\n*******  SAVING  *******\n\n");
 
     save(item, input.current.value, newImgUrl);
-
   }, [item]);
-  
+
   const cancelEdit = useCallback(() => {
     if (!input.current) return;
     setEditing(false);
@@ -203,87 +211,84 @@ function TodoItem(
     save(item, null, "");
   }, [item]);
 
-
-
   return (
-    <div>
-      <div
-        class="flex my-2 items-center"
-        {...{ "data-item-id": item.id! }}
-      >
-        {editing && (
-          <>
-            <input
-              class="border rounded w-full py-2 px-3 mr-4"
-              ref={input}
-              defaultValue={item.text}
-            />
-            <input
-              id="file-picker"
-              class="file-picker__input p-2 bg-blue-600 text-white rounded disabled:opacity-50"
-              type="file"
-              accept="image/*"
-              multiple
-              onChange={onFileSelect}
-              ref={imageLayout}
-            />
-            <label for="file-picker" class="file-picker__label">
-              <svg viewBox="0 0 24 24" class="file-picker__icon">
-                <path d="M19 7v3h-2V7h-3V5h3V2h2v3h3v2h-3zm-3 4V8h-3V5H5a2 2 0 00-2 2v12c0 1.1.9 2 2 2h12a2 2 0 002-2v-8h-3zM5 19l3-4 2 3 3-4 4 5H5z" />
-              </svg>
-            </label>
-            <button
-              class="p-2 rounded mr-2 disabled:opacity-50"
-              title="Save"
-              onClick={doSave}
-              disabled={busy}
-            >
-              💾
-            </button>
-            <button
-              class="p-2 rounded disabled:opacity-50"
-              title="Cancel"
-              onClick={cancelEdit}
-              disabled={busy}
-            >
-              🚫
-            </button>
-          </>
-        )}
-        {!editing && (
-          <>
-            <div class="flex flex-col w-full font-mono">
-              <div>
-                <p>
-                  {item.text}
-                </p>
-              </div>
-            </div>
-            <button
-              class="p-2 mr-2 disabled:opacity-50"
-              title="Edit"
-              onClick={() => setEditing(true)}
-              disabled={busy}
-            >
-              ✏️
-            </button>
-            <button
-              class="p-2 disabled:opacity-50"
-              title="Delete"
-              onClick={doDelete}
-              disabled={busy}
-            >
-              🗑️
-            </button>
-          </>
-        )}
-      </div>
-
       <div>
-        <ImageLayout files={files} />
-      </div>
+        <div
+          class="flex my-2 items-center"
+          {...{ "data-item-id": item.id! }}
+        >
+          {editing && (
+            <>
+              <input
+                class="border rounded w-full py-2 px-3 mr-4"
+                ref={input}
+                defaultValue={item.text}
+              />
+              <input
+                id="file-picker"
+                class="file-picker__input p-2 bg-blue-600 text-white rounded disabled:opacity-50"
+                type="file"
+                accept="image/*"
+                multiple
+                onChange={onFileSelect}
+                ref={fileInput}
+              />
+              <label for="file-picker" class="file-picker__label">
+                <svg viewBox="0 0 24 24" class="file-picker__icon">
+                  <path d="M19 7v3h-2V7h-3V5h3V2h2v3h3v2h-3zm-3 4V8h-3V5H5a2 2 0 00-2 2v12c0 1.1.9 2 2 2h12a2 2 0 002-2v-8h-3zM5 19l3-4 2 3 3-4 4 5H5z" />
+                </svg>
+              </label>
+              <button
+                class="p-2 rounded mr-2 disabled:opacity-50"
+                title="Save"
+                onClick={doSave}
+                disabled={busy}
+              >
+                💾
+              </button>
+              <button
+                class="p-2 rounded disabled:opacity-50"
+                title="Cancel"
+                onClick={cancelEdit}
+                disabled={busy}
+              >
+                🚫
+              </button>
+            </>
+          )}
+          {!editing && (
+            <>
+              <div class="flex flex-col w-full font-mono">
+                <div>
+                  <p>
+                    {item.text}
+                  </p>
+                </div>
+              </div>
+              <button
+                class="p-2 mr-2 disabled:opacity-50"
+                title="Edit"
+                onClick={() => setEditing(true)}
+                disabled={busy}
+              >
+                ✏️
+              </button>
+              <button
+                class="p-2 disabled:opacity-50"
+                title="Delete"
+                onClick={doDelete}
+                disabled={busy}
+              >
+                🗑️
+              </button>
+            </>
+          )}
+        </div>
 
-    </div>
+        <div>
+          <ImageLayout files={files} />
+        </div>
+      </div>
   );
 }
 
@@ -306,10 +311,10 @@ const renderImages = (fileUrls) => {
 function ImageLayout({ files }) {
   const [images, setImages] = useState();
 
-  useEffect(()=>{
-    console.log(`images changed:`)
-    console.dir(images)
-  }, [images])
+  useEffect(() => {
+    console.log(`images changed:`);
+    console.dir(images);
+  }, [images]);
 
   useEffect(() => {
     console.log(`From ImageLayout.tsx. files: ${files}`);
