4785c0e2f0bf556505cb707dbbc6540a44b80d23
Robert Talamantez <robert.m.talamantez@gmail.com>
2024-05-24 23:49:15 -0700

    reload on error

--- islands/LoaderIsland.tsx
+++ islands/LoaderIsland.tsx
@@ -6,44 +6,24 @@ import HomeContent from "./HomeContent.tsx";
 
 export default function LoaderIsland() {
   const [loading, setLoading] = useState(true);
-  const [retries, setRetries] = useState(0);
-  const maxRetries = 3;
-  const initialDelay = 1000;
-  const maxDelay = 10000;
 
   useEffect(() => {
-    const originalConsoleError = console.error;
-
-    const handleConsoleError = () => {
-      if (retries < maxRetries) {
-        const delay = Math.min(initialDelay * 2 ** retries, maxDelay);
-        console.warn(`Retrying in ${delay}ms...`);
-        setRetries(retries + 1);
-        setTimeout(() => {
-          setLoading(false);
-        }, delay);
-      } else {
-        console.error("Content loading failed after maximum retries");
-        // Refresh the page
-        window.location.reload();
-      }
+    // deno-lint-ignore no-explicit-any
+    const handleError = (event:any) => {
+      console.error("An error occurred:", event.error);
+      // Refresh the page on the first error
+      window.location.reload();
     };
 
-    console.error = (...args) => {
-      originalConsoleError(...args);
-      handleConsoleError();
-    };
-
-    globalThis.addEventListener("error", handleConsoleError);
+    globalThis.addEventListener("error", handleError);
 
     // Set loading state to false when the component mounts
     setLoading(false);
 
     return () => {
-      console.error = originalConsoleError;
-      globalThis.removeEventListener("error", handleConsoleError);
+      globalThis.removeEventListener("error", handleError);
     };
-  }, [retries]);
+  }, []);
 
   return (
     <div>
