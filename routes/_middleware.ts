ee9299c11a6fe8b10e7402931b4fb5799b741701
Robert Talamantez <robert.m.talamantez@gmail.com>
2024-06-12 13:13:20 -0700

    block php requests

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -4,23 +4,26 @@ import { serveDir } from "https://deno.land/std@0.140.0/http/file_server.ts";
 
 const staticDir = "static";
 
-export async function handler(
-  req: Request,
-  ctx: FreshContext,
-) {
+export async function handler(req: Request, ctx: FreshContext) {
   // Log the incoming request URL
   console.log(`[Middleware] Request URL: ${req.url}`);
 
   // Log the destination of the request
   console.log(`[Middleware] Destination: ${ctx.destination}`);
 
+  // Block all '.php' requests
+  const url = new URL(req.url);
+  const pathname = url.pathname;
+  if (pathname.endsWith(".php")) {
+    return new Response("Access denied. PHP requests are not allowed.", {
+      status: 403,
+    });
+  }
+
   // Pass the request to the next handler
   const resp = await ctx.next();
 
   // Set the appropriate MIME types based on the file extension
-  const url = new URL(req.url);
-  const pathname = url.pathname;
-
   if (pathname.endsWith(".js")) {
     resp.headers.set("Content-Type", "application/javascript");
   } else if (pathname.endsWith(".js.map")) {
