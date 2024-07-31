c4582c2844f68514ee1be5d686e0b7f349b8dd4e
Robert Talamantez <robert.m.talamantez@gmail.com>
2024-07-31 11:01:01 -0700

    Refine content

--- services/generateChaptersFromJSON.ts
+++ services/generateChaptersFromJSON.ts
@@ -1,6 +1,6 @@
 import intro from "../data/chapters/Introduction.json" with { type: "json" };
 import overview from "../data/chapters/An Overview of Mental Health Conditions.json" with { type: "json" };
-import specific from "../data/chapters/Specific Mental Health Conditions.json" with { type: "json" };
+// import specific from "../data/chapters/Specific Mental Health Conditions.json" with { type: "json" };
 import treatment from "../data/chapters/Treatment and Recovery.json" with { type: "json" };
 import housing from "../data/chapters/Housing.json" with { type: "json" };
 import employment from "../data/chapters/Employment.json" with { type: "json" };
@@ -8,68 +8,104 @@ import hipaa from "../data/chapters/Understanding HIPAA.json" with { type: "json
 import crisis from "../data/chapters/What to Do in a Crisis.json" with { type: "json" };
 import advocacy from "../data/chapters/Advocacy.json" with { type: "json" };
 import guardianship from "../data/chapters/Guardianship and Alternatives.json" with { type: "json" };
+import justice from "../data/chapters/The Criminal Justice System.json" with { type: "json" };
+import toolkit from "../data/chapters/Toolkit.json" with { type: "json" };
+import appendices from "../data/chapters/Appendicies.json" with { type: "json" };
+import acknowlegments from "../data/chapters/Acknowledgments.json" with { type: "json" };
+
 
 const chapters = {
   Chapters: [
     {
+      index: "0",
       title: intro.title,
       description: intro.description,
       sections: intro.sections,
       imageUrl: intro.imageUrl,
     },
     {
+      index: "1",
       title: overview.title,
       description: overview.description,
       sections: overview.sections,
       imageUrl: overview.imageUrl,
     },
     {
-      title: specific.title,
-      description: specific.description,
-      sections: specific.sections,
-      imageUrl: specific.imageUrl,
-    },
-    {
+      index: "2",
       title: treatment.title,
       description: treatment.description,
       sections: treatment.sections,
       imageUrl: treatment.imageUrl,
     },
     {
+      index: "3",
       title: crisis.title,
       description: crisis.description,
       sections: crisis.sections,
       imageUrl: crisis.imageUrl,
     },
     {
+      index: "4",
+      title: justice.title,
+      description: justice.description,
+      sections: justice.sections,
+      imageUrl: justice.imageUrl,
+    },
+    {
+      index: "5",
       title: employment.title,
       description: employment.description,
       sections: employment.sections,
       imageUrl: employment.imageUrl,
     },
     {
+      index: "6",
       title: housing.title,
       description: housing.description,
       sections: housing.sections,
       imageUrl: housing.imageUrl,
     },
     {
+      index: "7",
       title: guardianship.title,
       description: guardianship.description,
       sections: guardianship.sections,
       imageUrl: guardianship.imageUrl,
     },
     {
+      index: "8",
       title: hipaa.title,
       description: hipaa.description,
       sections: hipaa.sections,
       imageUrl: hipaa.imageUrl,
     },
     {
+      index: "9",
+      title: toolkit.title,
+      description: toolkit.description,
+      sections: toolkit.sections,
+      imageUrl: toolkit.imageUrl,
+    },
+    {
+      index: "10",
       title: advocacy.title,
       description: advocacy.description,
       sections: advocacy.sections,
       imageUrl: advocacy.imageUrl,
+    },
+    {
+      index: "11",
+      title: appendices.title,
+      description: appendices.description,
+      sections: appendices.sections,
+      imageUrl: appendices.imageUrl,
+    },
+    {
+      index: "12",
+      title: acknowlegments.title,
+      description: acknowlegments.description,
+      sections: acknowlegments.sections,
+      imageUrl: acknowlegments.imageUrl,
     },
   ],
 };
